f19f99d827d47c00bb0c95e9c9ef832679d29d09
MOHAMMAD AMMAR AZAM <93309473+ammar9525@users.noreply.github.com>
2024-08-26 03:26:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import Navbar from './components/navbar/Navbar'
+import Home from './components/home/Home'
+import Footer from './components/footer/Footer'
+import About from './components/about/About'
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Signup from './components/signup/Signup'
+import Signin from './components/signup/Signin'
+import Todo from './components/todo/Todo';
+import { authActions } from './store'
+import { useDispatch } from 'react-redux';
+import "./App.css";
 
-function App() {
+
+
+const App = () => {
+  const dispatch= useDispatch();
+
+
+  useEffect(() => {
+    const id =sessionStorage.getItem("id")
+    if(id){
+      dispatch(authActions.login());
+    }
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-home'>
+
+    <Router>
+
+    <Navbar />
+
+      <Routes>
+
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        
+        </Routes>
+
+       
+   
+      </Router>
+   
+      
+     
+      <Footer />
+    
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
